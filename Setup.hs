c662ef94f04534aa34618afbb64c1540d60cac8e
Lars Kuhtz <lars@kadena.io>
2025-03-07 15:46:04 -0800

    Cleanup Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -7,13 +7,27 @@ import Distribution.Simple
   , defaultMainWithHooks
   , simpleUserHooks
   )
-import Distribution.Simple.Program
-  ( ghcProgram
+import Distribution.Simple.BuildPaths
+  ( mkGenericSharedBundledLibName
+  , mkGenericSharedLibName
+  , mkGenericStaticLibName
+  , dllExtension
   )
+import Distribution.Simple.Compiler
 import Distribution.Simple.LocalBuildInfo
   ( LocalBuildInfo (localPkgDescr)
   , buildDir
   , compiler
+  )
+import Distribution.Simple.Program
+  ( Program
+  , ProgramDb
+  , addKnownPrograms
+  , configureAllKnownPrograms
+  , defaultProgramDb
+  , ghcProgram
+  , runDbProgram
+  , simpleProgram
   )
 import Distribution.Simple.Setup
   ( BuildFlags (buildVerbosity)
@@ -26,26 +40,37 @@ import Distribution.Simple.UserHooks
   )
 import Distribution.Simple.Utils
   ( notice
-  , copyFileVerbose
   , createDirectoryIfMissingVerbose
-  , rawSystemExit
   , info
+  , installExecutableFile
   )
-import System.Directory (getCurrentDirectory)
-import Distribution.Text (display)
-import Distribution.Simple.Compiler
 import Distribution.System (buildPlatform, buildOS, OS(..))
-import Distribution.Simple.BuildPaths
-  ( mkGenericSharedBundledLibName
-  , mkGenericSharedLibName
-  , mkGenericStaticLibName
-  , dllExtension
-  )
+import Distribution.Text (display)
+import System.Directory (getCurrentDirectory)
+
+-- -------------------------------------------------------------------------- --
+-- main
 
 main :: IO ()
 main =
   defaultMainWithHooks
     simpleUserHooks { buildHook = rustBuildHook }
+
+-- -------------------------------------------------------------------------- --
+-- Build Tools
+
+cargoProgram :: Program
+cargoProgram = simpleProgram "cargo"
+
+install_name_toolProgram :: Program
+install_name_toolProgram = simpleProgram "install_name_tool"
+
+programDb :: ProgramDb
+programDb = addKnownPrograms
+    [cargoProgram, install_name_toolProgram]
+    defaultProgramDb
+
+-- -------------------------------------------------------------------------- --
 
 rustBuildHook
   :: PD.PackageDescription
@@ -54,47 +79,78 @@ rustBuildHook
   -> BuildFlags
   -> IO ()
 rustBuildHook description localBuildInfo hooks flags = do
+  pdb <- configureAllKnownPrograms verbosity programDb
+
   -- run Rust build
-  -- FIXME add cargo and rust to program db during configure step
   -- FIXME: add `--target $TARGET` flag to support cross-compiling to $TARGET
   notice verbosity "Call `cargo build --release` to build a dependency written in Rust"
-  rawSystemExit (fromFlag $ buildVerbosity flags) "cargo"
+  runDbProgram verbosity cargoProgram pdb
     [ "build"
     , "--release"
-    , "--target-dir"
-    , rustTargetDir
+    , "--target-dir", rustTargetDir
     ]
 
   -- Install build results into cabal build directory
-  createDirectoryIfMissingVerbose verbosity True targetBuildDir
-  copyFileVerbose verbosity staticSource staticTarget
-  copyFileVerbose verbosity dynSource dynTarget
+  createDirectoryIfMissingVerbose verbosity True artifactsDir
+  installExecutableFile verbosity staticSource staticTarget
+  installExecutableFile verbosity staticSource staticTarget_
+  installExecutableFile verbosity dynSource dynTarget
+  installExecutableFile verbosity dynSource dynTarget_
 
-  -- seems to be needed for local use of template haskell
-  copyFileVerbose verbosity dynSource dynTarget_
-
-  when (buildOS == OSX) $
-    rawSystemExit verbosity "install_name_tool"
+  when (buildOS == OSX) $ do
+    runDbProgram verbosity install_name_toolProgram pdb
         [ "-id"
-        , "@rpath/" <> mkGenericSharedBundledLibName buildPlatform (compilerId c) targetLibname
+        , "@rpath" </> dynTargetFileName
         , dynTarget
+        ]
+    runDbProgram verbosity install_name_toolProgram pdb
+        [ "-id"
+        , "@rpath" </> dynTargetFileName_
+        , dynTarget_
         ]
 
   info verbosity "rustc compilation succeeded"
   buildHook simpleUserHooks description localBuildInfo hooks flags
  where
   verbosity = fromFlag $ buildVerbosity flags
-  c = compiler localBuildInfo
+  cid = compilerId $ compiler localBuildInfo
 
+  -- base builddir for this component within dist-newstyle
+  localBuildDir = buildDir localBuildInfo
+
+  -- directory where build artifacts are placed
+  artifactsDir = localBuildDir
+
+  -- library name stem
   sourceLibname = "plonk_verify"
   targetLibname = "C" <> sourceLibname
-  rustTargetDir = buildDir localBuildInfo <> "/rust-target"
-  sourceBuildDir = rustTargetDir <> "/release"
-  targetBuildDir = buildDir localBuildInfo
-  staticSource = sourceBuildDir <> "/" <> mkGenericStaticLibName sourceLibname
-  staticTarget = targetBuildDir <> "/" <> mkGenericStaticLibName targetLibname
-  dynSource = sourceBuildDir <> "/" <> "lib" <> sourceLibname <> "." <> dllExtension buildPlatform
-  dynTarget = targetBuildDir <> "/" <> mkGenericSharedBundledLibName buildPlatform (compilerId c) targetLibname
 
-  dynTarget_ = targetBuildDir <> "/" <> mkGenericSharedLibName buildPlatform (compilerId c) targetLibname
+  -- The target directory for rust builds
+  rustTargetDir = localBuildDir </> "rust-target"
+  -- directory where Rust builds place build artificts
+  rustArtifactDir = rustTargetDir <> "/release"
+
+  -- Static library names
+  staticSource = rustArtifactDir </> mkGenericStaticLibName sourceLibname
+  staticTarget = artifactsDir </> mkGenericStaticLibName targetLibname
+  staticTarget_ = artifactsDir </> mkGenericStaticLibName targetLibname
+
+  -- Dynamic library names
+  dynSource = rustArtifactDir </> "lib" <> sourceLibname <.> dllExtension buildPlatform
+  dynTargetFileName = mkGenericSharedBundledLibName buildPlatform cid targetLibname
+  dynTarget = artifactsDir </> dynTargetFileName
+
+  -- work around for cabal picking the wrong name during local builds with
+  -- template Haskell and in the repl
+  dynTargetFileName_ = mkGenericSharedLibName buildPlatform cid targetLibname
+  dynTarget_ = artifactsDir </> dynTargetFileName_
+
+-- -------------------------------------------------------------------------- --
+-- Utils
+
+(</>) :: FilePath -> FilePath -> FilePath
+a </> b = a <> "/" <> b
+
+(<.>) :: FilePath -> FilePath -> FilePath
+a <.> b = a <> "." <> b
 
